9609c4ce3cdb631825b69eb9a8375806511a7c20
Eugene <evgeniys1@tut.by>
2019-07-10 15:01:46 +0300

    delete comments

--- diploma_react-master/my-diploma/src/components/pages/coffepage.js
+++ diploma_react-master/my-diploma/src/components/pages/coffepage.js
@@ -49,24 +49,6 @@ class CoffeePage extends Component {
                             imgUrl='../../img/coffee_girl.jpg' 
                             imgAlt='girl'
                         />
-                        {/* <div className="row">
-                            <div className="col-lg-4 offset-2">
-                                <img className="shop__girl" src="../../img/coffee_girl.jpg" alt="girl"/>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="title">About our beans</div>
-                                <img className="beanslogo" src="../../logo/Beans_logo_dark.svg" alt="Beans logo"/>
-                                <div className="shop__text">
-                                    Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                                    <br/><br/>
-                                    Afraid at highly months do things on at. Situation recommend objection do intention<br/>
-                                    so questions. <br/>
-                                    As greatly removed calling pleased improve an. Last ask him cold feel<br/>
-                                    met spot shy want. Children me laughing we prospect answered followed. At it went<br/>
-                                    is song that held help face.
-                                </div>
-                            </div>
-                        </div> */}
                         <div className="line"></div>
                         <div className="row">
                             <SearchPanel/>
